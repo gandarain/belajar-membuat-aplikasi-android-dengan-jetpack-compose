846a8d37eace9c9c9cff4a8ef11c828643c73668
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-11-21 20:12:13 +0700

    #### Codelab Menyusun Item

--- 2. konsep-dasar-compose/2.1 latihan-dasar-compose/app/src/main/java/com/dicoding/lathandasarcompose/MainActivity.kt
+++ 2. konsep-dasar-compose/2.1 latihan-dasar-compose/app/src/main/java/com/dicoding/lathandasarcompose/MainActivity.kt
@@ -3,13 +3,19 @@ package com.dicoding.lathandasarcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dicoding.lathandasarcompose.ui.theme.LathanDasarComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            contentDescription = "Logo Jetpack Compose",
+            modifier = Modifier.size(80.dp)
+        )
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "Welcome to Docding!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
