42637a1ceee9c2dcf97c9bbda97d03d3fecb1c9f
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-11-27 01:14:50 +0700

    #### Create Category UI

--- 3. layout-pada-compose/3.3 JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
+++ 3. layout-pada-compose/3.3 JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dicoding.jetcoffee.components.CategoryItem
 import com.dicoding.jetcoffee.components.SearchBar
+import com.dicoding.jetcoffee.components.SectionText
+import com.dicoding.jetcoffee.model.dummyCategory
 import com.dicoding.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,13 +29,6 @@ class MainActivity : ComponentActivity() {
                 JetCoffeeApp()
             }
         }
-    }
-}
-
-@Composable
-fun JetCoffeeApp() {
-    Column {
-        Banner()
     }
 }
 
@@ -47,6 +44,37 @@ fun Banner(
             modifier = Modifier.height(160.dp)
         )
         SearchBar()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+    Column {
+        Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
