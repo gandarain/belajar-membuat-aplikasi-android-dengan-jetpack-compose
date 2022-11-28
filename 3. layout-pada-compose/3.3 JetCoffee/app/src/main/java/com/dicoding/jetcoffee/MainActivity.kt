d738ec5cda11873d1dac35917950b8908a1bacbe
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-11-28 19:56:27 +0700

    #### Create List Menu Item

--- 3. layout-pada-compose/3.3 JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
+++ 3. layout-pada-compose/3.3 JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -16,9 +18,13 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dicoding.jetcoffee.components.CategoryItem
+import com.dicoding.jetcoffee.components.MenuItem
 import com.dicoding.jetcoffee.components.SearchBar
 import com.dicoding.jetcoffee.components.SectionText
+import com.dicoding.jetcoffee.model.Menu
+import com.dicoding.jetcoffee.model.dummyBestSellerMenu
 import com.dicoding.jetcoffee.model.dummyCategory
+import com.dicoding.jetcoffee.model.dummyMenu
 import com.dicoding.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,6 +68,22 @@ fun CategoryRow(
 }
 
 @Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun CategoryRowPreview() {
     JetCoffeeTheme {
@@ -71,10 +93,13 @@ fun CategoryRowPreview() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
