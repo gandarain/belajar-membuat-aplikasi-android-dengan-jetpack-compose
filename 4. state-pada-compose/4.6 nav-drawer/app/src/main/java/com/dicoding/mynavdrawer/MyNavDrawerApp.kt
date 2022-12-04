a40fd19663d0eb1a8f834c4d61a022c1935b49b7
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-12-04 15:01:42 +0700

    #### Setup Nav Drawer Project

---
+++ 4. state-pada-compose/4.6 nav-drawer/app/src/main/java/com/dicoding/mynavdrawer/MyNavDrawerApp.kt
@@ -0,0 +1,17 @@
+package com.dicoding.mynavdrawer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.dicoding.mynavdrawer.ui.theme.MyNavDrawerTheme
+
+@Composable
+fun MyNavDrawerApp() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyNavDrawerTheme {
+        MyNavDrawerApp()
+    }
+}
